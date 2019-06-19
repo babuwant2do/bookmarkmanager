e8bf3a8062788f73ff2094c641a7b5046e001893
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2019-06-19 12:51:41 +0200

    comment on bookmark servoce

--- src/main/java/com/wordpress/babuwant2do/bookmarkmanager/service/BookmarkService.java
+++ src/main/java/com/wordpress/babuwant2do/bookmarkmanager/service/BookmarkService.java
@@ -2,24 +2,13 @@ package com.wordpress.babuwant2do.bookmarkmanager.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.wordpress.babuwant2do.bookmarkmanager.service.util.FileUtils;
 import com.wordpress.babuwant2do.bookmarkmanager.tree.BookmarkNodeDecorator;
@@ -27,7 +16,6 @@ import com.wordpress.babuwant2do.bookmarkmanager.tree.BookmarkNodeException;
 import com.wordpress.babuwant2do.bookmarkmanager.tree.builder.BookmarkProcessor;
 import com.wordpress.babuwant2do.bookmarkmanager.tree.builder.GroupSortedBookmarkProcessor;
 import com.wordpress.babuwant2do.bookmarkmanager.tree.builder.UniqueSortedBookmarkProcessor;
-import com.wordpress.babuwant2do.bookmarkmanager.web.rest.BookmarkResource;
 
 @Service
 public class BookmarkService {
@@ -44,10 +32,20 @@ public class BookmarkService {
 //		Jsou
 	}
 	
+	/**
+	 * Process input file and create bookmark list sorted unique in a single folder
+	 * @param file
+	 * @return - file name of the output file
+	 */
 	public String createUnique(File file){
 		return this.process(file, this.UniqueSortedBookmarkProcessor);
 	}
 	
+	/**
+	 * Process input file and create bookmark list sorted unique organized in folder
+	 * @param file
+	 * @return file name of the output file
+	 */
 	public String createGrouped(File file){
 		
 		return this.process(file, this.GroupSortedBookmarkProcessor);
